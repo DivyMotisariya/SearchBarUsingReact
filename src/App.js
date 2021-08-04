5555adbc3a802f77e9af9288dfb70c70bc92c61e
Divy Motisariya <motisariyadivy2@gmail.com>
2021-08-04 11:27:40 +0530

    Basic Search Bar to filter some data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import { DATA } from "./data/data";
+import FilterableProductTable from "./components/FilterableProductTable.component";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <FilterableProductTable products={DATA} />;
 }
 
 export default App;
